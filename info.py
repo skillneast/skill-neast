b35231acbca01a850be6b52afcbfeea2dff1142f
skillneast <allworkwithfahad@gmail.com>
2025-07-14 15:09:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ MONGODB_URI = environ.get("MONGODB_URI", "")
 STREAM_URL = environ.get("STREAM_URL", "")
 
 # This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url, So This Link Will Redirect To Stream Url.
-LINK_URL = environ.get("LINK_URL", "")
+LINK_URL = environ.get("LINK_URL", "https://skillneast.blogspot.com/p/s_7.html")
 
 # Others, Not Usefull
 PORT = environ.get("PORT", "8080")
