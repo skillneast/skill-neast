4809322f9a04a94663dad27beb5d291a92f2718e
skillneast <allworkwithfahad@gmail.com>
2025-10-20 01:51:04 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -1,81 +1,129 @@
-# Fully Asynchronous and Corrected database.py
-# Using motor for all operations to prevent blocking
-
+"from pymongo import MongoClient
 import motor.motor_asyncio
 from info import MONGODB_URI
+
+client = MongoClient(MONGODB_URI)
+db = client['videoplays']
+collection = db['playscount']
+
+def record_visit(user: int, count: int):
+    existing_visit = collection.find_one({
+        "user": user
+    })   
+    if not existing_visit:
+        collection.insert_one({
+            "user": user,
+            "count": count,
+            "withdraw": False
+        })
+    else:
+        user_data = {
+            "count": count
+        }
+        collection.update_one({"user": user}, {"$set": user_data})
+
+def record_withdraw(user, withdraw):
+    existing_visit = collection.find_one({
+        "user": user
+    })
+    if existing_visit:
+        user_data = {
+            "withdraw": withdraw
+        }
+        collection.update_one({"user": user}, {"$set": user_data})
+
+def get_count(user):
+    existing_visit = collection.find_one({
+        "user": user
+    })
+    if existing_visit:
+        return existing_visit["count"]
+    else: 
+        return None
+
+def get_withdraw(user):
+    existing_visit = collection.find_one({
+        "user": user
+    })
+    if existing_visit:
+        try:
+            return existing_visit["withdraw"]
+        except:
+            return False
+    else: 
+        return False
+
+
+class Database2:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id, name):
+        return dict(
+            id = id,
+            name = name,
+            b_name = None,
+            c_link = None,
+        )
+    
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+    
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id':int(id)})
+        return bool(user)
+
+checkdb = Database2(MONGODB_URI, "TechVJVideoPlayerBot")
 
 class Database:
     
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.users_col = self.db.users
-        self.plays_col = self.db.playscount # Consolidated collection
+        self.col = self.db.users
 
-    # ---- User Management Methods ----
+    def new_user(self, id, name):
+        return dict(
+            id = id,
+            name = name,
+            b_name = None,
+            c_link = None,
+        )
     
     async def add_user(self, id, name):
-        user = dict(id=id, name=name, b_name=None, c_link=None)
-        await self.users_col.insert_one(user)
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
     
     async def is_user_exist(self, id):
-        user = await self.users_col.find_one({'id': int(id)})
+        user = await self.col.find_one({'id':int(id)})
         return bool(user)
     
     async def total_users_count(self):
-        return await self.users_col.count_documents({})
+        count = await self.col.count_documents({})
+        return count
 
     async def get_all_users(self):
-        return self.users_col.find({})
+        return self.col.find({})
 
     async def delete_user(self, user_id):
-        await self.users_col.delete_many({'id': int(user_id)})
+        await self.col.delete_many({'id': int(user_id)})
 
     async def set_name(self, id, name):
-        await self.users_col.update_one({'id': int(id)}, {'$set': {'b_name': name}})
+        await self.col.update_one({'id': int(id)}, {'$set': {'b_name': name}})
 
     async def get_name(self, id):
-        user = await self.users_col.find_one({'id': int(id)})
-        return user.get('b_name') if user else None
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('b_name')
 
     async def set_link(self, id, link):
-        await self.users_col.update_one({'id': int(id)}, {'$set': {'c_link': link}})
+        await self.col.update_one({'id': int(id)}, {'$set': {'c_link': link}})
 
     async def get_link(self, id):
-        user = await self.users_col.find_one({'id': int(id)})
-        return user.get('c_link') if user else None
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('c_link')
 
-    # ---- Visit/Play Count Management Methods (ASYNC) ----
-    
-    async def record_visit(self, user_id: int):
-        await self.plays_col.update_one(
-            {'user': user_id},
-            {'$inc': {'count': 1}, '$setOnInsert': {'withdraw': False}},
-            upsert=True
-        )
-    
-    async def get_count(self, user_id: int):
-        data = await self.plays_col.find_one({'user': user_id})
-        return data.get('count', 0) if data else 0
-
-    async def reset_count(self, user_id: int):
-        await self.plays_col.update_one({'user': user_id}, {'$set': {'count': 0}})
-
-    # ---- Withdrawal Status Management Methods (ASYNC) ----
-
-    async def set_withdraw_status(self, user_id: int, status: bool):
-        await self.plays_col.update_one(
-            {'user': user_id},
-            {'$set': {'withdraw': status}},
-            upsert=True
-        )
-
-    async def get_withdraw_status(self, user_id: int):
-        data = await self.plays_col.find_one({'user': user_id})
-        return data.get('withdraw', False) if data else False
-
-# Create a single instance of the database to be used everywhere
-db = Database(MONGODB_URI, "VJStreamBotDB") 
-
-# For backward compatibility if other files are using 'checkdb'
-checkdb = db
+db = Database(MONGODB_URI, "VJVideoPlayerBot")"
